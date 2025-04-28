c57f06f91a0954ff91dc3a5d055bdd8a209a855a
이윤빈 <kanado20000@naver.com>
2025-04-29 01:03:49 +0900

    chore: 환경 변수 파일(.env) 추가, DB 연결 설정, Swagger 설정

---
+++ hertz-be/src/main/java/com/hertz/hertz_be/HertzBeApplication.java
@@ -0,0 +1,13 @@
+package com.hertz.hertz_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HertzBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HertzBeApplication.class, args);
+	}
+
+}
---
+++ hertz-be/src/test/java/com/hertz/hertz_be/HertzBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hertz.hertz_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HertzBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

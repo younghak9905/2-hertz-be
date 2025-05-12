1401b9944d103d6bbf004020c17c5fec702eb8fe
Chaewon Lee <87990439+zzeon9@users.noreply.github.com>
2025-05-12 22:17:50 +0900

    Chore: accessToken 테스트용 만료 기간 임시 단축 (#72)

--- hertz-be/src/main/java/com/hertz/hertz_be/global/auth/token/JwtTokenProvider.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/global/auth/token/JwtTokenProvider.java
@@ -28,7 +28,7 @@ public class JwtTokenProvider {
 
     public String createAccessToken(Long userId) {
         Date now = new Date();
-        Date expiryDate = Date.from(Instant.now().plus(1, ChronoUnit.HOURS));
+        Date expiryDate = Date.from(Instant.now().plus(1, ChronoUnit.MINUTES));
 
         return Jwts.builder()
                 .setSubject(userId.toString())

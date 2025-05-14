eae4ca10bc6d65fd344a8d7efa4484ce4696c62d
이윤빈 <81556800+kanado5385-k@users.noreply.github.com>
2025-05-14 20:39:26 +0900

    Feat/94/rt domain error (#119)
    * feat: 도메인 환경 변수에서 제외
    
    * fix: if 조건문 수정
    
    * feat: FE 환경에 따라 분기
    
    * feat: FE 환경에 따라 분기
    
    * feat: FE 도메인 환경변수 밖으로 이동
    
    * feat: FE 도메인 구체화
    
    * feat: FE 도메인 환경변수 제외시키기
    
    * feat: 환경변수 삭제
    
    * feat: 빌드형태 수정
    
    * feat: 서브도메인으로 수정
    
    * feat: backendUrl 제거해보기
    
    * feat: backendUrl 제거해보기
    
    * feat: RT를 httpOnly 쿠키에 담을 때 FE 클라이언트 서브 도메인 지정
    
    * feat: httpOnly 쿠키 빌드 방식 수정
    
    * feat: httpOnly 쿠키 빌드 방식 수정

--- hertz-be/src/main/java/com/hertz/hertz_be/domain/auth/controller/AuthController.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/domain/auth/controller/AuthController.java
@@ -44,10 +44,10 @@ public class AuthController {
         ResponseCookie responseCookie = ResponseCookie.from("refreshToken", newRefreshToken)
                 .maxAge(1209600)
                 .path("/")
-                .domain(isLocal ? null : ".hertz-tuning.com")  // isLocal일 경우 domain 생략
+                .domain(".hertz-tuning.com")   // 클라이언트 도메인 지정
                 .httpOnly(true)
                 .sameSite("None")
-                .secure(!isLocal)                               // isLocal=false면 secure 활성화
+                .secure(true)
                 .build();
 
         response.setHeader("Set-Cookie", responseCookie.toString());
--- hertz-be/src/main/java/com/hertz/hertz_be/domain/user/controller/UserController.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/domain/user/controller/UserController.java
@@ -42,10 +42,10 @@ public class UserController {
         responseCookie = ResponseCookie.from("refreshToken", userInfoResponseDto.getRefreshToken())
                 .maxAge(userInfoResponseDto.getRefreshSecondsUntilExpiry())
                 .path("/")
-                .domain(isLocal ? null : ".hertz-tuning.com")  // isLocal일 경우 domain 생략
+                .domain(".hertz-tuning.com")
                 .httpOnly(true)
                 .sameSite("None")
-                .secure(!isLocal)                               // isLocal=false면 secure 활성화
+                .secure(true)
                 .build();
 
         response.setHeader("Set-Cookie", responseCookie.toString());

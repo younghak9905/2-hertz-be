863494b0d329370f0bffcf2b84feb61f63eb8214
이윤빈 <81556800+kanado5385-k@users.noreply.github.com>
2025-06-10 00:47:13 +0900

    Hotfix: see 연결 도중에 예외 발생시 커스텀 예외 응답 추가 (#253)
    * feat: SSE
    
    * feat: 로그아웃시 SSE 연결 해제 로직 추가 및 예외 처리
    
    * feat: RT 만료 되었을 때 예외처리 로직 추가

--- hertz-be/src/main/java/com/hertz/hertz_be/global/sse/SseService.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/global/sse/SseService.java
@@ -1,7 +1,10 @@
 package com.hertz.hertz_be.global.sse;
 
+import com.hertz.hertz_be.domain.auth.exception.RefreshTokenInvalidException;
+import com.hertz.hertz_be.domain.auth.repository.RefreshTokenRepository;
 import com.hertz.hertz_be.domain.channel.exception.UserNotFoundException;
 import com.hertz.hertz_be.domain.user.repository.UserRepository;
+import com.hertz.hertz_be.global.common.ResponseCode;
 import com.hertz.hertz_be.global.common.SseEventName;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -18,17 +21,16 @@ import java.util.concurrent.ConcurrentHashMap;
 @RequiredArgsConstructor
 public class SseService {
     private final UserRepository userRepository;
-    // 무제한 유지
+    private final RefreshTokenRepository refreshTokenService;
+
     private static final Long TIMEOUT = 0L;
 
-    // userId -> emitter 매핑
     private final Map<Long, SseEmitter> emitters = new ConcurrentHashMap<>();
 
     public SseEmitter subscribe(Long userId) {
         userRepository.findById(userId)
                 .orElseThrow(UserNotFoundException::new);
 
-        // 기존 연결 제거
         if (emitters.containsKey(userId)) {
             emitters.get(userId).complete();
             emitters.remove(userId);
@@ -37,7 +39,6 @@ public class SseService {
         SseEmitter emitter = new SseEmitter(TIMEOUT);
         emitters.put(userId, emitter);
 
-        // 프론트에서 eventSource.close()를 호출하면 실행됨
         emitter.onCompletion(() -> {
             log.info("SSE 연결 종료: userId={}", userId);
             emitters.remove(userId);
@@ -49,14 +50,12 @@ public class SseService {
             emitters.remove(userId);
         });
 
-        // 최초 연결 시 ping 전송
         sendToClient(userId, SseEventName.PING.getValue(), "connect success");
         log.warn("connect success: userId={}", userId);
 
         return emitter;
     }
 
-    // 15초마다 heartbeat 전송 -> 헬스체크 역할
     @Scheduled(fixedRate = 15000)
     public void sendPeriodicPings() {
         emitters.forEach((userId, emitter) -> {
@@ -64,7 +63,6 @@ public class SseService {
                 emitter.send(SseEmitter.event()
                         .name(SseEventName.HEARTBEAT.getValue())
                         .data("heartbeat"));
-                //log.warn("heartbeat: userId={}", userId);
             } catch (IOException e) {
                 log.warn("heartbeat 전송 실패: userId={}, 연결 종료", userId);
                 emitter.complete();
@@ -74,6 +72,12 @@ public class SseService {
     }
 
     public boolean sendToClient(Long userId, String eventName, Object data) {
+        String storedToken = refreshTokenService.getRefreshToken(userId);
+        if (storedToken == null) {
+            sendErrorAndComplete(userId, ResponseCode.REFRESH_TOKEN_INVALID, "Access Token이 만료되었습니다. Refresh Token으로 재발급 요청이 필요합니다.");
+            return false;
+        }
+
         SseEmitter emitter = emitters.get(userId);
         if (emitter != null && data != null) {
             try {
@@ -89,6 +93,20 @@ public class SseService {
             }
         }
         return false;
+    }
+
+    public void sendErrorAndComplete(Long userId, String code, String message) {
+        SseEmitter emitter = emitters.get(userId);
+        try {
+            emitter.send(SseEmitter.event()
+                    .name("error")
+                    .data(Map.of("code", code, "message", message)));
+            emitter.complete();
+            emitters.remove(userId);
+        } catch (IOException ex) {
+            emitter.complete();
+            emitters.remove(userId);
+        }
     }
 
     public void disconnect(Long userId) {

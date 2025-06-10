7a872d0935592b423c11078d8003dad28b7991bf
daisy.lee <zzeonii0612@gmail.com>
2025-06-10 22:51:06 +0900

    feat: 튜닝 리포트 생성 관련 쿼리 추가

--- hertz-be/src/main/java/com/hertz/hertz_be/domain/tuningreport/repository/TuningReportRepository.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/domain/tuningreport/repository/TuningReportRepository.java
@@ -1,11 +1,14 @@
 package com.hertz.hertz_be.domain.tuningreport.repository;
 
+import com.hertz.hertz_be.domain.channel.entity.SignalRoom;
 import com.hertz.hertz_be.domain.tuningreport.entity.TuningReport;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface TuningReportRepository extends JpaRepository<TuningReport, Long> {
@@ -21,4 +24,6 @@ public interface TuningReportRepository extends JpaRepository<TuningReport, Long
     ) DESC
 """)
     Page<TuningReport> findAllOrderByTotalReactionDesc(Pageable pageable);
+
+    Optional<TuningReport> findBySignalRoom(SignalRoom signalRoom);
 }

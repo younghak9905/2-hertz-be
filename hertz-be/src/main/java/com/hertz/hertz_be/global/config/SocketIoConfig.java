844d403579273ffaad8b22f41efcf31b0e9a209c
daisy.lee <zzeonii0612@gmail.com>
2025-06-15 21:52:26 +0900

    feat: socket.io 환경변수 및 config 파일 추가

---
+++ hertz-be/src/main/java/com/hertz/hertz_be/global/config/SocketIoConfig.java
@@ -0,0 +1,33 @@
+package com.hertz.hertz_be.global.config;
+
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.Transport;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@RequiredArgsConstructor
+public class SocketIoConfig {
+
+    @Value("${socketio.server.hostname}")
+    private String hostname;
+
+    @Value("${socketio.server.port}")
+    private int port;
+
+    @Bean
+    public SocketIOServer socketIoServer() {
+        com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
+        config.setHostname(hostname);
+        config.setPort(port);
+        config.setOrigin("*");
+        config.setAllowCustomRequests(true);
+        config.setTransports(Transport.WEBSOCKET, Transport.POLLING);
+
+        return new SocketIOServer(config);
+
+    }
+
+}

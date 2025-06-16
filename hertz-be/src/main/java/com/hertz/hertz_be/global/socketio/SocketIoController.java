2d2ef422204fe8811ef5d3f00091a8782d7eb25d
daisy.lee <zzeonii0612@gmail.com>
2025-06-16 16:51:05 +0900

    chore: socket io 읽음 처리 관련 이벤트 리스너 추가, 연결 및 종료에 대한 join/leave 처리

--- hertz-be/src/main/java/com/hertz/hertz_be/global/socketio/SocketIoController.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/global/socketio/SocketIoController.java
@@ -4,13 +4,14 @@ import com.corundumstudio.socketio.SocketIOClient;
 import com.corundumstudio.socketio.SocketIOServer;
 import com.corundumstudio.socketio.listener.ConnectListener;
 import com.corundumstudio.socketio.listener.DisconnectListener;
-import com.hertz.hertz_be.domain.channel.dto.socketio.MessageRequest;
-import com.hertz.hertz_be.domain.channel.dto.socketio.MessageResponse;
+import com.hertz.hertz_be.global.socketio.dto.SocketIoMessageMarkRequest;
+import com.hertz.hertz_be.global.socketio.dto.SocketIoMessageRequest;
+import com.hertz.hertz_be.global.socketio.dto.SocketIoMessageResponse;
 import com.hertz.hertz_be.domain.channel.entity.SignalMessage;
+import com.hertz.hertz_be.domain.channel.repository.SignalRoomRepository;
 import com.hertz.hertz_be.global.auth.token.JwtTokenProvider;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
-import com.hertz.hertz_be.domain.channel.service.MessageService;
 
 import java.util.List;
 import java.util.Map;
@@ -19,24 +20,30 @@ import java.util.Map;
 @Slf4j
 public class SocketIoController {
     private final SocketIOServer server;
-    private final MessageService messageService;
+    private final SocketIoService messageService;
     private final JwtTokenProvider jwtTokenProvider;
+    private final SignalRoomRepository signalRoomRepository;
 
-    public SocketIoController(SocketIOServer server, MessageService messageService, JwtTokenProvider jwtTokenProvider) {
+    public SocketIoController(SocketIOServer server, SocketIoService messageService, JwtTokenProvider jwtTokenProvider, SignalRoomRepository signalRoomRepository) {
         this.server = server;
         this.messageService = messageService;
         this.jwtTokenProvider = jwtTokenProvider;
+        this.signalRoomRepository = signalRoomRepository;
 
         server.addConnectListener(listenConnected());
 
-        server.addEventListener("send_message", MessageRequest.class, (client, data, ackSender) -> {
+        server.addEventListener("send_message", SocketIoMessageRequest.class, (client, data, ackSender) -> {
             Long senderId = getUserIdFromClient(client);
 
             log.info("[{}] 채널 {} 에게 메세지 : {}", senderId, data.roomId(), data.message());
 
             SignalMessage savedMessage = messageService.saveMessage(data.roomId(), senderId, data.message());
+            server.getRoomOperations("room-" + data.roomId()).sendEvent("receive_message", SocketIoMessageResponse.from(savedMessage));
+        });
 
-            server.getRoomOperations("room-" + data.roomId()).sendEvent("receive_message", MessageResponse.from(savedMessage));
+        server.addEventListener("mark_as_read", SocketIoMessageMarkRequest.class, (client, data, ackSender) -> {
+            Long userId = getUserIdFromClient(client);
+            messageService.markMessageAsRead(data.roomId(), userId);
         });
     }
 
@@ -49,7 +56,20 @@ public class SocketIoController {
     public ConnectListener listenConnected() {
         return (client) -> {
             Map<String, List<String>> params = client.getHandshakeData().getUrlParams();
-            log.info(":: SocketIo Connect - " + params.toString() + " ::");
+            String token = params.get("token").get(0);
+            Long userId = jwtTokenProvider.getUserIdFromToken(token);
+
+            log.info(":: SocketIo Connect - userId : {} ", userId);
+
+            List<Long> joinedRoomIds = signalRoomRepository.findRoomIdsByUserId(userId);
+
+            for(Long roomId: joinedRoomIds) {
+                String roomKey = "room-" + roomId;
+                client.joinRoom(roomKey);
+                log.info("# user {} → {}", userId, roomKey);
+            }
+
+            client.set("userId", userId);
         };
     }
 
@@ -57,6 +77,14 @@ public class SocketIoController {
         return (client) -> {
             String sessionId = client.getSessionId().toString();
             log.info(":: SocketIo Disconnect - " + sessionId + " ::");
+
+            Long userId = client.get("userId");
+
+            for(String room : client.getAllRooms()) {
+                client.leaveRoom(room);
+                log.info("# user {} → {}", userId, room);
+            }
+
             client.disconnect();
         };
     }

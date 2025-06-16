42edd4c41d9a97c00238699ba53006606de6cb03
daisy.lee <zzeonii0612@gmail.com>
2025-06-16 16:46:40 +0900

    chore: socket io 관련 dto 파일명 변경

--- hertz-be/src/main/java/com/hertz/hertz_be/global/socketio/dto/SocketIoMessageResponse.java
+++ hertz-be/src/main/java/com/hertz/hertz_be/global/socketio/dto/SocketIoMessageResponse.java
@@ -1,17 +1,17 @@
-package com.hertz.hertz_be.domain.channel.dto.socketio;
+package com.hertz.hertz_be.global.socketio.dto;
 
 import com.hertz.hertz_be.domain.channel.entity.SignalMessage;
 
 import java.time.LocalDateTime;
 
-public record MessageResponse (
+public record SocketIoMessageResponse(
         Long roomId,
         Long senderId,
         String message,
         LocalDateTime sendAt
 ){
-    public static MessageResponse from (SignalMessage message) {
-        return new MessageResponse(
+    public static SocketIoMessageResponse from (SignalMessage message) {
+        return new SocketIoMessageResponse(
                 message.getSignalRoom().getId(),
                 message.getSenderUser().getId(),
                 message.getMessage(),
